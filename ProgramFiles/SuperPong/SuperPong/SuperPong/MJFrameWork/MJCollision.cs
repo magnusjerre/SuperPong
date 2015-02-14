dbe55592b9f074647b0dd3b2063f5ad2624a7c45
Magnus Jerre <magnus.jerre@gmail.com>
2015-02-14 12:55:07 +0100

    Created unit test
    - Extracted some of the collision methods from MJPhysicsBody into
    MJCollision.
    - Created unittests for horizontal, vertical and slanted overlaps

---
+++ ProgramFiles/SuperPong/SuperPong/SuperPong/MJFrameWork/MJCollision.cs
@@ -0,0 +1,214 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SuperPong.MJFrameWork
+{
+    public class MJCollision
+    {
+
+        public static Boolean MJLinesCross(Vector2 a1, Vector2 a2, Vector2 b1, Vector2 b2)
+        {
+            return MJLinesCross(a1, a2, b1, b2, 0.1f);
+        }
+
+        public static Boolean MJLinesCross(Vector2 a1, Vector2 a2, Vector2 b1, Vector2 b2, float delta)
+        {
+            if (bothLinesHorizontal(a1, a2, b1, b2, delta))
+                return MJHorizontalOverlap(a1, a2, b1, b2, delta);
+            if (lineIsVertical(a1, a2, delta))
+                return MJLinesCrossVertical(a1, a2, b1, b2, delta);
+            else if (lineIsVertical(b1, b2, delta))
+                return MJLinesCrossVertical(b1, b2, a1, a2, delta);
+            else
+                return MJLinesCrossDef(a1, a2, b1, b2, delta);
+        }
+
+        public static Boolean MJLinesCrossVertical(Vector2 v1, Vector2 v2, Vector2 a1, Vector2 a2, float delta)
+        {
+            if (LinesDoNotOverlapVertically(v1, v2, a1, a2) ||
+                LinesDoNotOverlapHorizontally(v1, v2, a1, a2))
+                return false;
+
+            if (lineIsVertical(a1, a2, delta))  //Handles dx==0
+            {
+                if (v1.X - delta < a1.X && a1.X < v1.X + delta)
+                    return MJVerticalOverlap(v1, v2, a1, a2, delta) || MJVerticalOverlap(a1, a2, v1, v2, delta);
+                return false;
+            }
+
+            //Confusingly, minYV is at the top of the screen, maxYV is at the bottom
+            float min = (float)(Math.Min(v1.Y, v2.Y)) - delta;
+            float max = (float)(Math.Max(v1.Y, v2.Y)) + delta;
+            float xOfVerticalLine = v1.X;
+
+            float dy = a2.Y - a1.Y;
+            float dx = a2.X - a1.X;
+
+            //Y = a * x + b
+            float a = dy / dx;
+            float b = a1.Y - a * a1.X;
+
+            float yCross = a * xOfVerticalLine + b;
+
+            return min < yCross && yCross < max;
+        }
+
+        private static bool LinesDoNotOverlapHorizontally(Vector2 a1,
+            Vector2 a2, Vector2 b1, Vector2 b2)
+        {
+            return
+                ((b1.X > a1.X && b1.X > a2.X) && (b2.X > a1.X && b2.X > a2.X))
+                    ||
+                ((b1.X < a1.X && b1.X < a2.X) && (b2.X < a1.X && b2.X < a2.X));
+        }
+
+        private static bool LinesDoNotOverlapVertically(Vector2 a1,
+            Vector2 a2, Vector2 b1, Vector2 b2)
+        {
+            return
+                ((b1.Y > a1.Y && b1.Y > a2.Y) && (b2.Y > a1.Y && b2.Y > a2.Y))
+                    ||
+                ((b1.Y < a1.Y && b1.Y < a2.Y) && (b2.Y < a1.Y && b2.Y < a2.Y));
+        }
+
+        public static Boolean MJLinesCrossDef(Vector2 a1, Vector2 a2, Vector2 b1, Vector2 b2, float delta)
+        {
+            float dyA = a2.Y - a1.Y;
+            float dyB = b2.Y - b1.Y;
+            float dxA = a2.X - a1.X;
+            float dxB = b2.X - b1.X;
+
+            //YA = aA * x + bA
+            //YB = aB * x + bB
+            float aA = dyA / dxA;
+            float bA = a2.Y - aA * a2.X;
+            float aB = dyB / dxB;
+            float bB = b2.Y - aB * b2.X;
+
+            if (aA - delta < aB && aB < aA + delta)
+                if (bA - delta < bB && bB < bA + delta)
+                    return slantedLinesOverlaps(a1, a2, b1, b2, delta);
+                else
+                    return false;
+
+            //YA = YB
+            //aA * x + bA = aB * x + bB
+            //(aA - aB) * x = bB - bA
+            //x = (bB - bA) / (aA - aB)
+            float xIntersection = (bB - bA) / (aA - aB);
+            float yIntersection = aA * xIntersection + bA;
+
+            float minXA = Min(a1.X, a2.X) - delta;
+            float maxXA = Max(a1.X, a2.X) + delta;
+            float minYA = Min(a1.Y, a2.Y) - delta;
+            float maxYA = Max(a1.Y, a2.Y) + delta;
+
+            float minXB = Min(b1.X, b2.X) - delta;
+            float maxXB = Max(b1.X, b2.X) + delta;
+            float minYB = Min(b1.Y, b2.Y) - delta;
+            float maxYB = Max(b1.Y, b2.Y) + delta;
+
+            return ((minXA < xIntersection && xIntersection < maxXA) &&
+                (minYA < yIntersection && yIntersection < maxYA)) &&
+                ((minXB < xIntersection && xIntersection < maxXB) &&
+                (minYB < yIntersection && yIntersection < maxYB));
+        }
+
+        public static Boolean MJVerticalOverlap(Vector2 a1, Vector2 a2, Vector2 b1, Vector2 b2, float delta)
+        {
+            if (lineIsVertical(a1, a2, delta) && lineIsVertical(b1, b2, delta))
+            {
+                if (a1.X - delta < b1.X && b1.X < a1.X + delta)
+                {
+                    float minYA = Min(a1.Y, a2.Y) - delta;
+                    float maxYA = Max(a1.Y, a2.Y) + delta;
+                    if ((minYA < b1.Y && b1.Y < maxYA) || (minYA < b2.Y && b2.Y < maxYA))
+                        return true;
+                    float minYB = Min(b1.Y, b2.Y) - delta;
+                    float maxYB = Max(b1.Y, b2.Y) + delta;
+                    if ((minYB < a1.Y && a1.Y < maxYB) || (minYB < a2.Y && a2.Y < maxYB))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public static Boolean MJHorizontalOverlap(Vector2 a1, Vector2 a2, Vector2 b1, Vector2 b2, float delta)
+        {
+            if (lineIsHorizontal(a1, a2, delta) && lineIsHorizontal(b1, b2, delta))
+            {
+                if (a1.Y - delta < b1.Y && b1.Y < a1.Y + delta)
+                {
+                    float minXA = Min(a1.X, a2.X) - delta;
+                    float maxXA = Max(a1.X, a2.X) + delta;
+                    if ((minXA < b1.X && b1.X < maxXA) || (minXA < b2.X && b2.X < maxXA))
+                        return true;
+                    float minXB = Min(b1.X, b2.X) - delta;
+                    float maxXB = Max(b1.X, b2.X) + delta;
+                    if ((minXB < a1.X && a1.X < maxXB) || (minXB < a2.X && a2.X < maxXB))
+                        return true;
+                }
+            }
+            return false;
+        }
+
+        public static Boolean slantedLinesOverlaps(Vector2 a1, Vector2 a2, Vector2 b1, Vector2 b2, float delta)
+        {
+            if (lineIsHorizontal(a1, a2, delta) || 
+                lineIsHorizontal(b1, b2, delta) || 
+                lineIsVertical(a1, a2, delta) || 
+                lineIsVertical(b1, b2, delta))
+                return false;
+
+            float minXA = Min(a1.X, a2.X) - delta;
+            float maxXA = Max(a1.X, a2.X) + delta;
+            float minYA = Min(a1.Y, a2.Y) - delta;
+            float maxYA = Max(a1.Y, a2.Y) + delta;
+
+            if (((minXA < b1.X && b1.X < maxXA) && (minYA < b1.Y && b1.Y < maxYA)) ||
+                ((minXA < b2.X && b2.X < maxXA) && (minYA < b2.Y && b2.Y < maxYA)))
+                return true;
+
+            float minXB = Min(b1.X, b2.X) - delta;
+            float maxXB = Max(b1.X, b2.X) + delta;
+            float minYB = Min(b1.Y, b2.Y) - delta;
+            float maxYB = Max(b1.Y, b2.Y) + delta;
+
+            if (((minXB < a1.X && a1.X < maxXB) && (minYA < a1.Y && a1.Y < maxXB)) ||
+                ((minXB < a2.X && a2.X < maxXB) && (minYB < a2.Y && a2.Y < maxYB)))
+                return true;
+
+            return false;
+        }
+
+        public static Boolean bothLinesHorizontal(Vector2 a1, Vector2 a2, Vector2 b1, Vector2 b2, float delta)
+        {
+            return lineIsHorizontal(a1, a2, delta) && lineIsHorizontal(b1, b2, delta);
+        }
+
+        public static Boolean lineIsHorizontal(Vector2 a1, Vector2 a2, float delta)
+        {
+            float dy = a2.Y - a1.Y;
+            return -delta < dy && dy < delta;
+        }
+
+        public static Boolean lineIsVertical(Vector2 a1, Vector2 a2, float delta)
+        {
+            float dx = a2.X - a1.X;
+            return -delta < dx && dx < delta;
+        }
+
+        public static float Min(float a, float b)
+        {
+            return a < b ? a : b;
+        }
+
+        public static float Max(float a, float b)
+        {
+            return a > b ? a : b;
+        }
+    }
+}
---
+++ ProgramFiles/SuperPong/UnitTestSuperPong/UnitTestMJCollision.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SuperPong.MJFrameWork;
+using Microsoft.Xna.Framework;
+
+namespace UnitTestSuperPong
+{
+    [TestClass]
+    public class UnitTestMJCollision
+    {
+        Vector2 a1 = new Vector2(100, 50);
+        Vector2 a2 = new Vector2(100, 200);
+        Vector2 b1 = new Vector2(100, 100);
+        Vector2 b2 = new Vector2(300, 100);
+        Vector2 c1 = new Vector2(50, 150);
+        Vector2 c2 = new Vector2(200, 150);
+        Vector2 d1 = new Vector2(250, 150);
+        Vector2 d2 = new Vector2(350, 50);
+        Vector2 e1 = new Vector2(350, 150);
+        Vector2 e2 = new Vector2(350, 300);
+        Vector2 f1 = new Vector2(450, 150);
+        Vector2 f2 = new Vector2(700, 150);
+        Vector2 g1 = new Vector2(500, 150);
+        Vector2 g2 = new Vector2(600, 150);
+        Vector2 h1 = new Vector2(500, 200);
+        Vector2 h2 = new Vector2(650, 200);
+        Vector2 i1 = new Vector2(350, 400);
+        Vector2 i2 = new Vector2(400, 300);
+        Vector2 j1 = new Vector2(350, 400);
+        Vector2 j2 = new Vector2(550, 400);
+        Vector2 k1 = new Vector2(300, 450);
+        Vector2 k2 = new Vector2(500, 450);
+        Vector2 l1 = new Vector2(150, 500);
+        Vector2 l2 = new Vector2(350, 400);
+        Vector2 m1 = new Vector2(150, 350);
+        Vector2 m2 = new Vector2(200, 700);
+        Vector2 n1 = new Vector2(750, 150);
+        Vector2 n2 = new Vector2(800, 150);
+        Vector2 o1 = new Vector2(350, 250);
+        Vector2 o2 = new Vector2(350, 350);
+        Vector2 p1 = new Vector2(600, 300);
+        Vector2 p2 = new Vector2(700, 400);
+        Vector2 q1 = new Vector2(750, 450);
+        Vector2 q2 = new Vector2(850, 550);
+        Vector2 r1 = new Vector2(650, 350);
+        Vector2 r2 = new Vector2(800, 500);
+        Vector2 s1 = new Vector2(900, 200);
+        Vector2 s2 = new Vector2(850, 250);
+        Vector2 t1 = new Vector2(850, 250);
+        Vector2 t2 = new Vector2(800, 300);
+        Vector2 u1 = new Vector2(350, 200);
+        Vector2 u2 = new Vector2(350, 250);
+        Vector2 v1 = new Vector2(600, 550);
+        Vector2 v2 = new Vector2(600, 650);
+        Vector2 w1 = new Vector2(600, 700);
+        Vector2 w2 = new Vector2(600, 750);
+        Vector2 x1 = new Vector2(750, 600);
+        Vector2 x2 = new Vector2(750, 650);
+        Vector2 aa1 = new Vector2(100, 100);
+        Vector2 aa2 = new Vector2(200, 200);
+        Vector2 bb1 = new Vector2(150, 150);
+        Vector2 bb2 = new Vector2(250, 250);
+        Vector2 cc1 = new Vector2(50, 50);
+        Vector2 cc2 = new Vector2(300, 300);
+        Vector2 dd1 = new Vector2(50, 250);
+        Vector2 dd2 = new Vector2(400, 250);
+
+        [TestMethod]
+        public void TestOverlapsHorizontal()
+        {
+            float delta = 0.2f;
+            Assert.IsTrue(MJCollision.MJHorizontalOverlap(f1, f2, g1, g2, delta));
+            Assert.IsTrue(MJCollision.MJHorizontalOverlap(g1, g2, f1, f2, delta));
+            Assert.IsTrue(MJCollision.MJHorizontalOverlap(g1, g2, g2, g1, delta));
+            Assert.IsFalse(MJCollision.MJHorizontalOverlap(f1, f2, h1, h2, delta));
+            Assert.IsFalse(MJCollision.MJHorizontalOverlap(f1, f2, n1, n2, delta));
+            Assert.IsFalse(MJCollision.MJHorizontalOverlap(b1, b2, a1, a2, delta));
+        }
+
+        [TestMethod]
+        public void TestOverlapsVertical()
+        {
+            float delta = 0.2f;
+            Assert.IsTrue(MJCollision.MJVerticalOverlap(e1, e2, o1, o2, delta));
+            Assert.IsTrue(MJCollision.MJVerticalOverlap(e1, e2, e1, e2, delta));
+            Assert.IsTrue(MJCollision.MJVerticalOverlap(e1, e2, e2, e1, delta));
+            Assert.IsTrue(MJCollision.MJVerticalOverlap(e1, e2, u1, u2, delta));
+            Assert.IsTrue(MJCollision.MJVerticalOverlap(u1, u2, e1, e2, delta));
+            Assert.IsFalse(MJCollision.MJVerticalOverlap(v1, v2, w1, w2, delta));
+            Assert.IsFalse(MJCollision.MJVerticalOverlap(v1, v2, x1, x2, delta));
+            Assert.IsFalse(MJCollision.MJVerticalOverlap(a1, a2, b1, b2, delta));
+        }
+
+        [TestMethod]
+        public void TestOverlapsSlanted()
+        {
+            float delta = 0.2f;
+            Assert.IsTrue(MJCollision.slantedLinesOverlaps(aa1, aa2, bb1, bb2, delta));
+            Assert.IsTrue(MJCollision.slantedLinesOverlaps(aa1, aa2, aa1, aa2, delta));
+            Assert.IsTrue(MJCollision.slantedLinesOverlaps(aa1, aa2, cc1, cc2, delta));
+            Assert.IsTrue(MJCollision.slantedLinesOverlaps(cc1, cc2, aa1, aa2, delta));
+
+            Assert.IsFalse(MJCollision.slantedLinesOverlaps(p1, p2, q1, q2, delta));
+            Assert.IsFalse(MJCollision.slantedLinesOverlaps(p1, p2, d1, d2, delta));
+            Assert.IsFalse(MJCollision.slantedLinesOverlaps(p1, p2, a1, a2, delta));
+            Assert.IsFalse(MJCollision.slantedLinesOverlaps(p1, p2, b1, b2, delta));
+            Assert.IsFalse(MJCollision.slantedLinesOverlaps(dd1, dd2, cc1, cc2, delta));
+        }
+
+
+    }
+}
