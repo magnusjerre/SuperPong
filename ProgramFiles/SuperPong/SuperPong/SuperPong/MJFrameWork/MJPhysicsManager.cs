6ad5973e9ebe50e13b713f691b458153f7d66519
Magnus Jerre <magnus.jerre@gmail.com>
2015-03-18 15:13:20 +0100

    No speed increase/decrease from moving paddle

--- ProgramFiles/SuperPong/SuperPong/SuperPong/MJFrameWork/MJPhysicsManager.cs
+++ ProgramFiles/SuperPong/SuperPong/SuperPong/MJFrameWork/MJPhysicsManager.cs
@@ -187,13 +187,19 @@ namespace SuperPong.MJFrameWork
             float v2nFinal = 0f;
             if (body1.IsStatic)
             {
-                v1nFinal = v1n;
-                v2nFinal = 2 * v1n - v2n;
+                //The commented out code removes the speed increase/decrease from moving static
+                //v1nFinal = v1n;
+                //v2nFinal = 2 * v1n - v2n;
+                v1nFinal = 0;
+                v2nFinal = -v2n;
             }
             else if (body2.IsStatic)
             {
-                v1nFinal = 2 * v2n - v1n;
-                v2nFinal = v2n;
+                //The commented out code removes the speed increase/decrease from moving static
+                //v1nFinal = 2 * v2n - v1n;
+                //v2nFinal = v2n;
+                v1nFinal = -v1n;
+                v2nFinal = 0;
             }
             else
             {
